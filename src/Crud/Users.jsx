d660e0d573f137ecc50eb09ea016be3758f7fc5a
Nikhil-Mandia <131294988+Nikhil-Mandia@users.noreply.github.com>
2023-10-14 10:30:27 +0530

    Update Users.jsx

--- src/Crud/Users.jsx
+++ src/Crud/Users.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";   // very imp ask in interview
+import { useState,useEffect } from "react";   
 import axios from "axios";
 import style from "./home.module.css"
 import { Link } from "react-router-dom";
